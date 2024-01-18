f1adb3db257e1775df3239b6094bf0f0f5a971e8
mabrur007 <mirmd.mabrur@gmail.com>
2024-01-18 12:36:55 +0600

    3 cards added dynamically

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,27 @@
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
 
-function Card({data}) {
+function Card({ data }) {
   return (
-    <div className="relative overflow-hidden w-60 h-72 rounded-[40px] px-5 py-10 bg-zinc-900/90 text-white ">
-          <FaRegFileAlt className="h-6" />
-      <p className="mt-4 leading-tight text-sm">{ data.bodyText }</p>
-          
-          <div className="footer absolute w-full left-0 bottom-0">
-              <div className="flex justify-between items-center px-8 py-4">
-                  <p>{ data.fileSize}</p>
-                  <span className="w-7 h-7 bg-zinc-600 flex justify-center items-center rounded-full">
-                    <LuDownload color="#fff" size=".8em"/>
-                  </span>
-              </div>
-              <div className="tag w-full bg-green-600 py-4">
-                  <h3 className="text-center text-sm ">Download Now</h3>
-              </div>
+    <div className="relative overflow-hidden w-60 h-72 rounded-[40px] px-5 py-10 bg-zinc-900/90 text-white shrink-0">
+      <FaRegFileAlt />
+      <p className="mt-4 text-sm w-[90%]">{data.bodyText}</p>
+
+      <div className="footer absolute w-full left-0 bottom-0">
+        <div className="flex justify-between items-center px-8 py-4">
+          <p>{data.fileSize}</p>
+          <span className="w-7 h-7 bg-zinc-600 flex justify-center items-center rounded-full">
+            {data.close ? <IoClose /> : <LuDownload color="#fff" size=".8em" />}
+          </span>
+        </div>
+        {data.tag.isOpen && (
+          <div className="tag w-full bg-green-600 py-4">
+            <h3 className="text-center text-sm ">{data.tag.tagTitle}</h3>
           </div>
+        )}
+        
+      </div>
     </div>
   )
 }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -3,9 +3,29 @@ import Card from "./Card"
 function Foreground() {
   const data = [
     {
-      bodyText: "I am changing the text using the props drilling.",
+      bodyText: "I am changing the text using props drilling from Foreground to Card Component 1",
       fileSize: ".7mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      bodyText: "I am changing the text using props drilling from Foreground to Card Component 2",
+      fileSize: ".9mb",
       close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      bodyText: "I am changing the text using props drilling from Foreground to Card Component 3",
+      fileSize: ".5mb",
+      close: false,
       tag: {
         isOpen: true,
         tagTitle: "Download Now",
@@ -15,7 +35,7 @@ function Foreground() {
   ]
   return (
     <div>
-          <div className='fixed w-full h-full top-0 left-0 z-[2] p-10'>
+          <div className='fixed w-full h-full top-0 left-0 z-[2] p-10 flex flex-wrap gap-10'>
             {
               data.map((item, i) => (
                 <Card data={item} key={i}/>
