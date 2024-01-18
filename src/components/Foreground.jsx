aa1762bbf19524cff0a800adf59f5841fb98a0e3
mabrur007 <mirmd.mabrur@gmail.com>
2024-01-18 12:09:34 +0600

    passing data from fg to card compo

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,26 @@
 import Card from "./Card"
 
 function Foreground() {
+  const data = [
+    {
+      bodyText: "I am changing the text using the props drilling.",
+      fileSize: ".7mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+  ]
   return (
     <div>
-          <div className='fixed w-full h-full top-0 left-0 z-[2]'>
-              <Card />
+          <div className='fixed w-full h-full top-0 left-0 z-[2] p-10'>
+            {
+              data.map((item, i) => (
+                <Card data={item} key={i}/>
+              ))
+            }
           </div>
     </div>
   )
